d07049888457759672bc874a3cecde6e884a04eb
Michael Scott <mscotteagle21@gmail.com>
2024-04-02 21:58:27 -0500

    started creating routes to different pages, added logic for user model

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const mediaRoutes = require('./mediaRoutes');
+
+
+router.use('/users', userRoutes);
+router.use('/media', mediaRoutes);
+
+
+module.exports = router;
