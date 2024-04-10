1eafbd1ab4799a827e5ce0acc7d8ee3c779b2cb0
Zariah <zaripull18@gmail.com>
2024-04-09 20:55:31 -0500

    working in models index

--- models/index.js
+++ models/index.js
@@ -2,8 +2,19 @@ const Movies = require('./Movies');
 const Shows = require('./Shows');
 const User = require('./User');
 
-module.exports = {
-    User,
-    Movies,
-    Shows,
-};
+User.hasMany(Shows, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  User.hasMany(Movies, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  Shows.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  Movies.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Movies, Shows };
